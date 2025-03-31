84eef83f14502ee69148c334aff1ef10fcdefcb6
RIDHIKA VERMA <ridhikav29@gmail.com>
2025-03-31 19:25:17 +0530

    Solution to Find the Leaders in an Array

---
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void findleaders(int arr[],int size){
+    int max_from_right=arr[size-1];
+    for(int i=size-2;i>=0;i--){
+        if(arr[i]>max_from_right){
+            max_from_right=arr[i];
+            printf("%d",max_from_right);
+        }
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=n-1;i++){
+        scanf("%d",&arr[i]);
+    }
+    findleaders(arr,n);
+    return 0;
+}
